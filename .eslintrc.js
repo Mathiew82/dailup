79859f048ae31d1992cea89d0ea3878e8ef3a68c
Mathiew82 <amateo1982@yahoo.es>
2022-03-06 00:43:39 +0100

    Creado el archivo de configuración para eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'vue'
+  ],
+  'rules': {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2]
+  }
+}
