94f1005f04e09cc31020684fdaafc94c6eda8f5a
Mathiew82 <amateo1982@yahoo.es>
2022-03-06 03:54:33 +0100

    Probando a ver si fallan los tests e2e en la pipeline

--- cypress/integration/App.js
+++ cypress/integration/App.js
@@ -1,6 +1,6 @@
 describe('App tests e2e', () => {
   it('front page can be opened', () => {
     cy.visit('http://localhost:3000/')
-    cy.contains('Agregar usuario')
+    cy.contains('asfdfsaf')
   })
 })
