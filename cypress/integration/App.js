70e52edd1c2791a16d505b2643a9309581be1290
Mathiew82 <amateo1982@yahoo.es>
2022-03-06 04:47:18 +0100

    Modificado el puerto con el que se levanta la preview de la app

--- cypress/integration/App.js
+++ cypress/integration/App.js
@@ -1,6 +1,6 @@
 describe('App tests e2e', () => {
   it('front page can be opened', () => {
-    cy.visit('http://localhost:8080/')
+    cy.visit('http://localhost:4173/')
     cy.contains('Agregar usuario')
   })
 })
