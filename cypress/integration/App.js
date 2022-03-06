b36f63ab40fe2141ba29decb406d54f76267c9d8
Mathiew82 <amateo1982@yahoo.es>
2022-03-06 04:37:21 +0100

    Actualizada la pipeline

--- cypress/integration/App.js
+++ cypress/integration/App.js
@@ -1,6 +1,6 @@
 describe('App tests e2e', () => {
   it('front page can be opened', () => {
-    cy.visit('http://localhost:3000/')
-    cy.contains('asfdfsaf')
+    cy.visit('http://localhost:8080/')
+    cy.contains('Agregar usuario')
   })
 })
