380fe8150b8836f51b33624ebd91ce8b1ca360d1
Mathiew82 <amateo1982@yahoo.es>
2022-03-19 20:27:07 +0100

    Creada la store de Pinia para user

---
+++ src/stores/user.js
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    lastId: 0,
+    activeUser: {
+      id: 0,
+      name: 'Activa el turno de un usuario'
+    },
+    users: [],
+    confirmedUsers: false
+  }),
+  actions: {
+    addUser(userName) {
+      if (userName) {
+        this.users.push({
+          id: this.lastId.value++,
+          name: userName,
+          time: 0
+        })
+      }
+    },
+    removeUser(userId) {
+      const currentUser = this.users.find(user => user.id === userId)
+      const position = this.users.indexOf(currentUser)
+      this.users.splice(position, 1)
+    },
+    setActiveUser(user) {
+      this.activeUser.id = user.id
+      this.activeUser.name = user.name
+    },
+    setUserTime(userId, time) {
+      const currentUser = this.users.find(user => user.id === userId)
+      currentUser.time = time
+    }
+  },
+})
