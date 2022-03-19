b0087c5ac2bfd9608c66909656095e55fa84fe7b
Mathiew82 <amateo1982@yahoo.es>
2022-03-19 20:28:13 +0100

    Eliminados todos los providers y props y eventos de los componentes Aside y Content

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <template>
   <div class="app">
-    <Aside @confirmed-users="confirmUsers" />
-    <Content :confirmedUsers="confirmedUsers" />
+    <Aside />
+    <Content />
   </div>
 </template>
 
 <script setup>
-import { provide, ref } from 'vue'
 import Aside from './components/Aside.vue'
 import Content from './components/Content.vue'
-import useUsers from './hooks/useUsers'
-import useCounters from './hooks/useCounters'
-
-const { activeUser, users, addUser, removeUser, setActiveUser, setUserTime } =
-  useUsers()
-const { start, stop } = useCounters()
-
-provide('activeUser', activeUser)
-provide('users', users)
-provide('addUser', addUser)
-provide('removeUser', removeUser)
-provide('setActiveUser', setActiveUser)
-provide('setUserTime', setUserTime)
-provide('start', start)
-provide('stop', stop)
-
-const confirmedUsers = ref(false)
-
-const confirmUsers = () => {
-  confirmedUsers.value = true
-}
 </script>
 
 <style lang="scss">
