f09a6ccf8a49eb7ecf0650ef792a058910144304
Mathiew82 <amateo1982@yahoo.es>
2022-03-27 05:35:10 +0200

    Agregados los alias en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +15,11 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'node_modules'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
